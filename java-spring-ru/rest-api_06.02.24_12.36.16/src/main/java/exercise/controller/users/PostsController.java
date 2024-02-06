8774167419ebaefae6a635f1468a48d62505baf8
@hexlet/cli <support@hexlet.io>
2024-02-06 12:41:48 +0400

    submit java-spring-ru/rest-api_06.02.24_12.36.16

---
+++ java-spring-ru/rest-api_06.02.24_12.36.16/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,33 @@
+package exercise.controller.users;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import exercise.model.Post;
+import exercise.Data;
+
+// BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    private List<Post> posts = Data.getPosts();
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/users/{userId}/posts")
+    public Post create(@PathVariable Integer userId, @RequestBody Post post) {
+        post.setUserId(userId);
+        posts.add(post);
+        return post;
+    }
+
+    @GetMapping("/users/{userId}/posts")
+    public List<Post> show(@PathVariable Integer userId) {
+        return posts.stream()
+                .filter(p -> userId.equals(p.getUserId())).toList();
+    }
+}
+// END
