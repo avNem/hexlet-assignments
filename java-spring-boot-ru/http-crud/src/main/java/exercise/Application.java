8db14ecf86ec79575d8f6d65a8e5985a6ae5259b
@hexlet/cli <support@hexlet.io>
2025-02-12 15:19:25 +0400

    submit java-spring-boot-ru/http-crud

--- java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -5,13 +5,7 @@ import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import exercise.model.Post;
 
@@ -38,7 +32,7 @@ public class Application {
                 .findFirst();
     }
 
-    @PostMapping("")
+    @PostMapping("/posts")
     public Post createPost(@RequestBody Post post) {
         posts.add(post);
         return post;
